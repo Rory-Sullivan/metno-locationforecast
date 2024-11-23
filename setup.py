27c9f747003d0ff559f7985b3c06b05bf8bc4f8b
Rory Sullivan <codingrory@gmail.com>
2024-11-22 18:14:58 -0800

    Updates dependencies and minimum requirements
    BREAKING CHANGE
    
    Updates minimum supported Python version to 3.9.
    
    Updates minimum supported version of requests library to 2.25.1.
    
    Adds requirement for tzdata >=2020.5.
    
    Bumps package version to v2.0.0.
    
    Updates all development dependencies to latest versions.

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 """Setup file for metno-locationforecast package."""
 
-from setuptools import setup, find_packages
 import pathlib
+
+from setuptools import find_packages, setup
 
 here = pathlib.Path(__file__).parent.resolve()
 
@@ -10,7 +11,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="metno-locationforecast",
-    version="1.2.0",
+    version="2.0.0",
     description="A Python interface for MET Norway's Locationforecast 2.0 service.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -25,10 +26,11 @@ setup(
         "Topic :: Software Development :: Libraries",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Programming Language :: Python :: 3 :: Only",
     ],
     keywords=[
@@ -46,18 +48,19 @@ setup(
     ],
     packages=find_packages(include=["metno_locationforecast", "metno_locationforecast.*"]),
     package_data={"metno_locationforecast": ["py.typed"]},
-    python_requires=">=3.6",
-    install_requires=["requests>=2.20.0"],
+    python_requires=">=3.9",
+    install_requires=["requests>=2.25.1", "tzdata>=2020.5"],
     extras_require={
         "dev": [
-            "black==24.3.0",
-            "flake8==3.8.3",
-            "mypy==0.782",
-            "pydocstyle==5.0.2",
-            "pytest==5.4.3",
-            "coverage==5.2",
+            "black==24.10.0",
+            "check-manifest==0.50",
+            "coverage==7.6.7",
+            "flake8==7.1.1",
+            "mypy==1.13.0",
+            "pydocstyle==6.3.0",
+            "pytest==8.3.3",
             "twine==5.1.1",
-            "check-manifest==0.42",
+            "tzdata==2020.5",
         ]
     },
     project_urls={
